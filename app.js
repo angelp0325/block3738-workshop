3818443fb07307b9ebf3e7b1bb6d07745cc1de3d
Angel Polanco <angelepolanco325@gmail.com>
2025-11-22 19:06:21 -0500

    app added

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
 import express from "express";
+import employeesRouter from "#api/employees.js";
 const app = express();
 export default app;
 
-// TODO: this file!
+// === Middleware ===
+app.use(express.json());
+
+// === Routes ===
+app.get("/", (req, res) => {
+  res.send("Welcome to the Fullstack Employees API.");
+});
+
+app.use("/employees", employeesRouter);
+
+// === Error handling ===
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ error: "Internal Server Error" });
+});
